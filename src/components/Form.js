8e2223cb5e69459d1ef13ceb28fa11b667591e7d
Gaganv2001 <gaganv2001@gmail.com>
2023-07-04 22:04:40 +0530

    changed URL

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000'; // Replace with your base URL
+const BASE_URL = 'https://webpagescrapper.onrender.com'; 
 
 
 export default function Form() {
